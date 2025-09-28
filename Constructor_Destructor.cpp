144d8e5b6d135818906301e160a8d5396d2c0889
choimk531 <choimk03@naver.com>
2025-09-28 17:22:35 +0900

    Add files via upload

---
+++ Constructor_Destructor.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+class Parent {
+public:
+    Parent() { cout << "Parent 생성자" << endl; }
+    virtual ~Parent() { cout << "Parent 소멸자" << endl; }
+};
+
+class Child : public Parent {
+public:
+    Child() { cout << "Child 생성자" << endl; }
+    ~Child() { cout << "Child 소멸자" << endl; }
+};
+
+int main() {
+    Child c;
+}
